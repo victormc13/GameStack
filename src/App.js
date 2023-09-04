127f7d3f2643ceeae256787d1d13164b7b6311b3
victormc13 <viktorsb14@gmail.com>
2023-09-03 19:32:24 -0500

    Add and import routes elements from categories folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Category from './routes/Category';
+import RecentlyReleased from './categories/RecentlyReleased';
+import Popular from './categories/Popular';
+import HallOfFame from './categories/HallOfFame';
+import Upcoming from './categories/Upcoming';
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Category />} />
+        <Route path='/recently-released' element={<RecentlyReleased />} />
+        <Route path='/popular' element={<Popular />} />
+        <Route path='/hall-of-fame' element={<HallOfFame />} />
+        <Route path='/upcoming' element={<Upcoming />} />
       </Routes>
     </div>
 
