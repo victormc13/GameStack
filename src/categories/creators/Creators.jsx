b8c2d85a970c91eb3d8eddcbad80f392cd0187ac
victormc13 <viktorsb14@gmail.com>
2023-09-12 16:56:46 -0500

    Create the Creators component that renders the creators page

---
+++ src/categories/creators/Creators.jsx
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import getCreators from '../../redux/byCreators/fetchCreators';
+
+const Creators = () => {
+  const { creators } = useSelector((state) => state.creators);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCreators());
+  }, [dispatch]);
+
+  return (
+    <div className="creators-section bg-white">
+      <h2 className="text-slate-900 font-bold">Platforms</h2>
+      <ul className="creators-container text-white">
+        {creators.map((creator) => (
+          <li key={creator.id} className="hover:bg-sky-800 cursor-pointer">
+            <NavLink to={`${creator.id}`}>
+              <p className="text-2xl mb-3">{creator.name}</p>
+              <img src={creator.image_background} alt={creator.name} width="200px" />
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Creators;
