1412fcbbc4a3e8fbd2f1a00b440252386b833393
victormc13 <viktorsb14@gmail.com>
2023-09-12 17:18:21 -0500

    Refactor to get the image creator and change the background of the page

--- src/categories/creators/Creators.jsx
+++ src/categories/creators/Creators.jsx
@@ -13,13 +13,13 @@ const Creators = () => {
 
   return (
     <div className="creators-section bg-white">
-      <h2 className="text-slate-900 font-bold">Platforms</h2>
+      <h2 className="text-slate-900 font-bold">Creators</h2>
       <ul className="creators-container text-white">
         {creators.map((creator) => (
           <li key={creator.id} className="hover:bg-sky-800 cursor-pointer">
             <NavLink to={`${creator.id}`}>
               <p className="text-2xl mb-3">{creator.name}</p>
-              <img src={creator.image_background} alt={creator.name} width="200px" />
+              <img src={creator.image} alt={creator.name} width="200px" />
             </NavLink>
           </li>
         ))}
