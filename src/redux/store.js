2bbc350f80519e56e2da2335f0c5a8d87fdf8c89
victormc13 <viktorsb14@gmail.com>
2023-09-12 16:37:27 -0500

    Imports and add the creatorsReducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import genresReducer from './byGenres/genresSlice';
 import gameDetailsReducer from './gameDetails/gameDetailsSlice';
 import platformsReducer from './byPlatforms/platformsSlice';
+import creatorsReducer from './byCreators/creatorsSlice';
 
 const store = configureStore({
   reducer: {
     genres: genresReducer,
     game: gameDetailsReducer,
     platforms: platformsReducer,
+    creators: creatorsReducer,
   },
 });
 
