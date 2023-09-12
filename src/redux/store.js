a362820edc0dbf3e34b5a529d4b54f8d9c360585
victormc13 <viktorsb14@gmail.com>
2023-09-12 11:04:31 -0500

    Import and add the platformsRecuer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import genresReducer from './byGenres/genresSlice';
 import gameDetailsReducer from './gameDetails/gameDetailsSlice';
+import platformsReducer from './byPlatforms/platformsSlice';
 
 const store = configureStore({
   reducer: {
     genres: genresReducer,
     game: gameDetailsReducer,
+    platforms: platformsReducer,
   },
 });
 
