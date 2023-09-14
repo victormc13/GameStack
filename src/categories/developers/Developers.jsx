0760883d00537e28ed78dbf9fb8f9a71d870a951
victormc13 <viktorsb14@gmail.com>
2023-09-13 22:49:47 -0500

    Get the developers list from the store and render to the page

--- src/categories/developers/Developers.jsx
+++ src/categories/developers/Developers.jsx
@@ -12,7 +12,17 @@ const Developers = () => {
 
   console.log('Developesr page for now', developers);
   return (
-    <div>Developers page for now</div>
+    <div className="developers-section bg-white">
+      <h2 className="text-slate-900 font-bold">Developers</h2>
+      <ul className="developers-container text-white">
+        {developers.map((developer) => (
+          <li key={developer.id} className="hover:bg-sky-800 cursor-pointer">
+            <p className="text-2xl mb-3">{developer.name}</p>
+            <img src={developer.image_background} alt={developer.name} width="200px" />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
